b45fc30e933bf2ab27f35dd69ca0fa338daa9c04
nguuuquaaa <nguuuquaaa@gmail.com>
2019-03-22 00:28:10 +0700

    add requirements and setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="flat",
+    author="nguuuquaaa",
+    url="https://github.com/nguuuquaaa/flat",,
+    version="0.0.1",
+    packages=["flat"],
+    license="MIT",
+    description="Facebook chat (Messenger) wrapper written in python."
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require={
+        "pillow": [
+            "pillow"
+        ]
+    },
+    python_requires=">=3.5.3"
+)
