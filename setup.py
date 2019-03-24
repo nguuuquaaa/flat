3279ee81ef9d7b8f003278922ef77e031c13f175
nguuuquaaa <nguuuquaaa@gmail.com>
2019-03-24 21:54:11 +0700

    "I forgot" - Araki Hirohiko

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 with open("flat/__init__.py") as f:
-    m = re.search(r"__version__ \= \"(\d+\.\d+\.\d+)\"")
+    m = re.search(r"__version__ \= \"(\d+\.\d+\.\d+)\"", f.read())
     version = m.group(1)
 
 setuptools.setup(
